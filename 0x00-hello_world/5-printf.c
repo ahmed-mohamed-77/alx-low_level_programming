6d24936d6816d7e0b3124b2e4a8e6ae7f6828ab9
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-16 10:25:28 -0800

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+* main - use printf "function for printing text".
+* Return: 0
+**/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
