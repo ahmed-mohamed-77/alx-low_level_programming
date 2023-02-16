e0fbeadb736a5a432d82f61f9ab07d84ed1cb882
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-16 10:12:05 -0800

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /*
 * main - A programe that prints line function
-* and
 * return: 0 (sucsess)
 */
 int main(void)
 {
-put("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
