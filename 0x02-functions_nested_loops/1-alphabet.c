4b7d5d27304230e449b1d985571b55d160ca74d8
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-21 11:17:38 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Description - print alphabets in lower case
+* Return: 0 (Sucsess)
+**/
+
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+return (0);
+}
