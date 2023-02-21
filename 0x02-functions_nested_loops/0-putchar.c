38aae907c9dabae3c85ae045a147ad65917b6d2e
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-21 09:44:41 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - use putchar function
+* Description  (loop function)
+* Return: 0 (success)
+**/
+
+int main(void)
+{
+char i;
+for (i = 'A'; i < 'Z'; i++)
+{
+	putchar(i);
+}
+
+return (0);
+}
