550b982979dc061039cb67ae331bf78a2d350fa4
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-21 15:37:31 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,12 +2,12 @@
 
 /**
 * time_table - prints the 9 times table, starting with 0
-* decription: times_table
 * Return: empty output
 */
 
 void times_table(void)
 {
+
 	int x, y, z, u, d;
 
 	for (x = 0; x <= 9; x++)
