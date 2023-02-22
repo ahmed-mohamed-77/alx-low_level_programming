fab18e9b45a31acb7475255cb9fcc6e0d08337e1
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-21 17:28:23 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
+/**
+ * print_fib - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * @len: the length of with the fib sequance should print to.
+ *
+ * Return: void
+ */
+void print_fib(int len)
+{
+	unsigned int target, i;
+
+	unsigned int former_l, former_r,;
+	unsigned int latter_l, latter_r;
+	unsigned int fib_l, fib_r;
+
+	f = 1;
+	l = 2;
+	target = len - 2;
+	for (i = 0 ; i < target ; i++)
+	{
+		fib = f + l;
+		printf("%d : ", i + 4);
+		printf("%.0Lf" , fib);
+		f = l;
+		l = fib;
+		if (i < target - 1)
+			printf("\n");
+	}
+	putchar('\n');
+}
+
 
 /**
-* main - main block
-* Description: computes and prints the sum of all the multiples of 3
-* 5 below 1024 (excluded), followed by a new line
-* Return: 0
-*/
-
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-	printf("%lu", bef);
-	
-	for (i = 1; i < 91; i++)
-	{
-	printf(",%lu", aft);
-	aft += bef;
-	bef = aft - bef;
-	}
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-	for (i = 92; i < 99; ++i)
-	{
-	printf((", %lu", aft1 + (aft2 / l));
-	printf("%lu", aft2 % l);
-	aft1 = aft1 + bef1;
-	bef1 = aft1 - bef1;
-	aft2 = aft2 + bef2;
-	bef2 = aft2 - bef2;
-	}
-	printf("\n");
+	print_fib(98);
 	return (0);
 }
+
