4e0a491c517db1647c0d766a2e152f9afb986a79
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-02-23 09:54:17 -0800

    0x03. C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,38 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void positive_or_negative(int i);
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d is ", n);
+
+	if (n > 0)
+	{
+		printf("positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("zero\n");
+	}
+	else
+	{
+		printf("negative\n");
+	}
+	return (0);
+}
+
 
 #endif /* MAIN_H */
