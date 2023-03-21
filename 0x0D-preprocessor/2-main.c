99be3b53d8495033d0242bd3eb7a669bdf941653
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2023-03-21 08:55:41 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+* prints the name of the file
+* use the macro (__FILE__) to print the name of the file
+*/
+int main(){
+
+    printf("%s\n", __FILE__);
+
+    return 0;
+}
